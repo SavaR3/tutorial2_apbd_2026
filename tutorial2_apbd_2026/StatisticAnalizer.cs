e8e8402ada6a8a44a001f8b0cd09eb1f7db846aa
Sava Rachynskyi <s33286@pjwstk.edu.pl>
2026-03-11 11:39:19 +0100

    method calculateaverage

--- tutorial2_apbd_2026/StatisticAnalizer.cs
+++ tutorial2_apbd_2026/StatisticAnalizer.cs
@@ -4,11 +4,18 @@ namespace tutorial2_apbd_2026;
 
 public class StatisticAnalizer
 {
-    private static List<IntegerType> listOfIntegers = new List<IntegerType>();
+    private static List<int> listOfIntegers = new List<int>();
     private static int average;
     private static int max;
     private static int min;
     private static int sum;
-    
-    
+
+    static double CalculateAverage(List<int> OfIntegers)
+    {
+        for (int i = 0; i < OfIntegers.Count; i++)
+        {
+            sum += OfIntegers[i];
+        }
+        return sum/= OfIntegers.Count;
+    }
 }
