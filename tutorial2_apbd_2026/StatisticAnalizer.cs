946d4c01128550f297fa18d46f2af01e6fb332d2
Sava Rachynskyi <s33286@pjwstk.edu.pl>
2026-03-11 11:28:00 +0100

    insertion of fields to class Statistic analyther

--- tutorial2_apbd_2026/StatisticAnalizer.cs
+++ tutorial2_apbd_2026/StatisticAnalizer.cs
@@ -1,6 +1,14 @@
-﻿namespace tutorial2_apbd_2026;
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace tutorial2_apbd_2026;
 
 public class StatisticAnalizer
 {
+    private static List<IntegerType> listOfIntegers = new List<IntegerType>();
+    private static int average;
+    private static int max;
+    private static int min;
+    private static int sum;
+    
     
 }
